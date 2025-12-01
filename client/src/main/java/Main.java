eb1b790e21e1696069fcf494e34e52eba0aa718a
Bear-Incorporated <chosun.kodiak@gmail.com>
2025-12-01 11:55:22 -0700

    Got Repl & State to compile.

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,13 +1,25 @@
-import chess.*;
-
+import chess.ChessGame;
+import chess.ChessPiece;
+import ui.*;
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
         System.out.println("Options:");
-        System.out.println("\"help\" or \"h\" Displays text informing the user what actions they can take.");
-        System.out.println("\"quit\" or \"q\" Exits the program.");
-        System.out.println("\"login <USERNAME> <PASSWORD>\" or \"l <USERNAME> <PASSWORD>\" to Log in.");
-        System.out.println("\"register <USERNAME> <PASSWORD> <EMAIL>\" or \"r <USERNAME> <PASSWORD> <EMAIL>\" Registers a new user.");
+        System.out.println("\"help\" or \"h\" - Displays text informing the user what actions they can take.");
+        System.out.println("\"quit\" or \"q\" - Exits the program.");
+        System.out.println("\"login <USERNAME> <PASSWORD>\" or \"l <USERNAME> <PASSWORD>\" - Logs in.");
+        System.out.println("\"register <USERNAME> <PASSWORD> <EMAIL>\" or \"r <USERNAME> <PASSWORD> <EMAIL>\" - Registers a new user.");
+
+
+
+
+
+        new Repl().run();
+        //new Repl(serverUrl).run();
     }
+
+
+
+
 }
--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -47,6 +47,7 @@ public class EscapeSequences {
     public static final String SET_BG_COLOR_WHITE = SET_BG_COLOR + "15m";
     public static final String RESET_BG_COLOR = UNICODE_ESCAPE + "[49m";
 
+
     public static final String WHITE_KING = " ♔ ";
     public static final String WHITE_QUEEN = " ♕ ";
     public static final String WHITE_BISHOP = " ♗ ";
@@ -60,6 +61,10 @@ public class EscapeSequences {
     public static final String BLACK_ROOK = " ♜ ";
     public static final String BLACK_PAWN = " ♟ ";
     public static final String EMPTY = " \u2003 ";
+
+    public static final String LOGO = " ♕ ";
+
+    public static final String RESET = RESET_TEXT_BOLD_FAINT + RESET_TEXT_ITALIC + RESET_TEXT_UNDERLINE + RESET_TEXT_BLINKING + RESET_TEXT_COLOR + RESET_BG_COLOR;
 
     public static String moveCursorToLocation(int x, int y) { return UNICODE_ESCAPE + "[" + y + ";" + x + "H"; }
 }
