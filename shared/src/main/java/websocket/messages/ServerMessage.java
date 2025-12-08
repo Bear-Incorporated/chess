42fd1c6743fa6091dd84c113bd3560684bcd56a0
Bear-Incorporated <chosun.kodiak@gmail.com>
2025-12-08 13:40:41 -0700

    Updated the shared websocket code.

--- shared/src/main/java/websocket/messages/ServerMessage.java
+++ shared/src/main/java/websocket/messages/ServerMessage.java
@@ -10,6 +10,7 @@ import java.util.Objects;
  */
 public class ServerMessage {
     ServerMessageType serverMessageType;
+    String message;
 
     public enum ServerMessageType {
         LOAD_GAME,
@@ -18,6 +19,10 @@ public class ServerMessage {
     }
 
     public ServerMessage(ServerMessageType type) {
+        this.serverMessageType = type;
+    }
+
+    public ServerMessage(ServerMessageType type, String message) {
         this.serverMessageType = type;
     }
 
